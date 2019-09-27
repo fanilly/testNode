b48ec4b224f785abb66af003460d7a4d3b8e7697
fanilly <iallenaugustine@gmail.com>
2019-09-27 18:34:22 +0800

    add gm

--- src/admin/controller/user.js
+++ src/admin/controller/user.js
@@ -1,12 +1,28 @@
 const Base = require('./base.js')
 const fs = require('fs')
 const _ = require('lodash')
+const path = require('path')
+const gm = require('gm')
 
 module.exports = class extends Base {
-  uploadUserAvatarAction(){
-    let avatar = this.file('file')
-    const RESOURCE_PATH = path.join(think.ROOT_PATH, 'www')
-    console.log(avatar.path)
-    console.log(think.RESOURCE_PATH)
+  uploadUserAvatarAction() {
+    const avatar = this.file('file')
+    if (think.isEmpty(avatar)) return this.fail(0, '保存失败')
+    const avatarPath = path.join(think.ROOT_PATH, `www/static/user/avatar/${think.uuid(12)}${this.ctx.state.userId}@@@@@@@@@@@@@@@@.${_.last(_.split(avatar.path, '.'))}`)
+    let temp = fs.readFileSync(avatar.path)
+    gm(avatar.path).size(function(err, info){
+      if(err){
+
+      }
+      console.log(err)
+      console.log('--------------------')
+      console.log(info.width, info.height)
+    })
+    console.log(avatar)
+    return this.success(avatar)
+    console.log(temp.toString())
+    console.log(this.ctx.state.userId)
+    // console.log(avatar)
+    console.log(avatarPath)
   }
 }
